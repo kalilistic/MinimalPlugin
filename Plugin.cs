6e1ef2cd09db7fa30cf946cae74b4df8e6503150
kalilistic <35899782+kalilistic@users.noreply.github.com>
2022-04-24 20:44:26 -0400

    add test window

--- Plugin.cs
+++ Plugin.cs
@@ -1,25 +1,43 @@
+using System.Diagnostics.CodeAnalysis;
+using Dalamud.Interface.Colors;
+using Dalamud.IoC;
 using Dalamud.Logging;
 using Dalamud.Plugin;
+using ImGuiNET;
 
-namespace MinimalPlugin
+namespace MinimalPlugin;
+
+/// <inheritdoc />
+[SuppressMessage("Usage", "CA1816:Dispose methods should call SuppressFinalize")]
+public class Plugin: IDalamudPlugin
 {
-    /// <inheritdoc />
-    public class Plugin: IDalamudPlugin
+    /// <summary>
+    /// Minimal plugin.
+    /// </summary>
+    public Plugin()
     {
-        /// <summary>
-        /// Minimal plugin.
-        /// </summary>
-        public Plugin()
-        {
-            PluginLog.Log("Plugin started.");
-        }
-
-        /// <inheritdoc />
-        public void Dispose()
-        {
-        }
-
-        /// <inheritdoc />
-        public string Name => "MinimalPlugin";
+        PluginLog.Log("Plugin started.");
+        PluginInterface.UiBuilder.Draw += UiBuilderOnDraw;
     }
+
+    [PluginService]
+    private static DalamudPluginInterface PluginInterface { get; set; } = null!;
+
+    private void UiBuilderOnDraw()
+    {
+        ImGui.PushStyleColor(ImGuiCol.WindowBg, ImGuiColors.ParsedBlue);
+        ImGui.Begin("Window");
+        ImGui.Text("Test Window");
+        ImGui.End();
+        ImGui.PopStyleColor();
+    }
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        PluginInterface.UiBuilder.Draw -= UiBuilderOnDraw;
+    }
+
+    /// <inheritdoc />
+    public string Name => "MinimalPlugin";
 }
